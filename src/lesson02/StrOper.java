25ba89c723f413c94ff504fb1860ce070f201f33
Den Ushakov <den.ushakov@gmail.com>
2018-09-13 23:47:48 +0300

    String task

---
+++ src/lesson02/StrOper.java
@@ -0,0 +1,25 @@
+package lesson02;
+
+
+
+    class EveryNth {
+        public static String everyNth(String str, int n) {
+            String result = "";
+            for (int i=0; i<str.length(); i = i + n) {
+                result = result + str.charAt(i);
+            }
+            return result;
+        }
+        public static void main(String[] args) {
+            System.out.println(everyNth("Miracle",2));
+            System.out.println(everyNth("abcdefg",2));
+            System.out.println(everyNth("abcdefg",4));
+        }
+    }
+
+
+
+
+
+
+
